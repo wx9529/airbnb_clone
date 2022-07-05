51b487f9134b38d6c8540f2dac879aa3278efd75
wx9529 <xinwang213z@gmail.com>
2022-07-05 12:10:06 -0700

    Add images

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,17 @@
+import Image from "next/image"
+
 function Header() {
   return (
-    <header>
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md-px-10">
       <h1>I am the header</h1>
 
-      <div>
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
-          alt="" />
+          objectFit="contain"
+          objectPosition="left"
+        />
       </div>
       <div></div>
       <div></div>
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['links.pareact.com'],
+    domains: ['links.papareact.com'],
   },
 }
 
