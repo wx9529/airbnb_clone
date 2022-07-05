e7ba738cbf9879edad0eac023911ec25c1a2675d
wx9529 <xinwang213z@gmail.com>
2022-07-05 11:45:03 -0700

    Add header component

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header>
+      <h1>I am the header</h1>
+
+      <div>
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          alt="" />
+      </div>
+      <div></div>
+      <div></div>
+
+    </header>
+  )
+}
+
+export default Header
